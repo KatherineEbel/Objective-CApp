a3380a6be4d6acd0bafa31fcc6d1671b50f998d3
Katherine Ebel <kathyebel@me.com>
2017-01-21 17:04:25 -0500

    Initial Commit

---
+++ WhatsforDinner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhatsforDinner
+//
+//  Created by Katherine Ebel on 1/21/17.
+//  Copyright © 2017 Katherine Ebel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhatsforDinner/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WhatsforDinner
+//
+//  Created by Katherine Ebel on 1/21/17.
+//  Copyright © 2017 Katherine Ebel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
